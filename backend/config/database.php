96ac906e320e9cf29cd6a3099c85b2bf7cf7271e
marvely975 <tanguilamarvely96@gmail.com>
2026-09-24 21:44:27 +0000

    cambio

--- backend/config/database.php
+++ backend/config/database.php
@@ -10,7 +10,7 @@ function database(): PDO
 
     $host = getenv('DB_HOST') ?: '127.0.0.1';
     $port = getenv('DB_PORT') ?: '3306';
-    $name = getenv('DB_NAME') ?: 'jambeli_control';
+    $name = getenv('DB_NAME') ?: 'UEjambeli_control';
     $user = getenv('DB_USER') ?: 'root';
     $password = getenv('DB_PASSWORD') ?: '';
     $dsn = "mysql:host={$host};port={$port};dbname={$name};charset=utf8mb4";
